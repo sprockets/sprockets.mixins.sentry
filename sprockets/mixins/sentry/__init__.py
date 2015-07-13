0053ff19835b085b5608bbea1aa87b3c781c104b
Dave Shawley <daves@aweber.com>
2015-07-13 10:59:08 -0400

    Introduce self.sentry_client + refactor a little.

--- sprockets/mixins/sentry/__init__.py
+++ sprockets/mixins/sentry/__init__.py
@@ -30,7 +30,7 @@ class SentryMixin(object):
 
     def initialize(self):
         self.sentry_tags = {}
-        if not hasattr(self.application, SENTRY_CLIENT):
+        if self.sentry_client is None:
             sentry_dsn = os.environ.get('SENTRY_DSN')
             if sentry_dsn:
                 setattr(self.application, SENTRY_CLIENT,
@@ -45,33 +45,33 @@ class SentryMixin(object):
         return values
 
     def _handle_request_exception(self, e):
-        if isinstance(e, web.HTTPError):
+        if isinstance(e, web.HTTPError) or self.sentry_client is None:
             return super(SentryMixin, self)._handle_request_exception(e)
 
-        if hasattr(self.application, SENTRY_CLIENT):
-            duration = math.ceil((time.time() - self.request._start_time) * 1000)
-            data = {}
-            if hasattr(self, 'request'):
-                data = {'request':
-                            {'url': self.request.full_url(),
-                             'method': self.request.method,
-                             'data': self.request.body,
-                             'query_string': self.request.query,
-                             'cookies': self.request.headers.get('Cookie', {}),
-                             'headers': dict(self.request.headers)},
-                        'logger': 'sprockets.mixins.sentry',
-                        'modules': self._get_module_data()}
-            kwargs = {'extra':
-                          {'handler': '{0}.{1}'.format(__name__,
-                                                       self.__class__.__name__),
-                           'env': self._strip_uri_passwords(dict(os.environ)),
-                           'http_host': self.request.host,
-                           'remote_ip': self.request.remote_ip},
-                      'time_spent': duration}
-            if self.sentry_tags:
-                kwargs.update({'tags': self.sentry_tags})
-            self.application.sentry_client.captureException(True, data=data,
-                                                            **kwargs)
+        duration = math.ceil((time.time() - self.request._start_time) * 1000)
+        kwargs = {
+            'extra': {
+                'handler': '{0}.{1}'.format(__name__, self.__class__.__name__),
+                'env': self._strip_uri_passwords(dict(os.environ))},
+            'time_spent': duration}
+        if hasattr(self, 'request'):
+            kwargs['data'] = {
+                'request': {
+                    'url': self.request.full_url(),
+                    'method': self.request.method,
+                    'data': self.request.body,
+                    'query_string': self.request.query,
+                    'cookies': self.request.headers.get('Cookie', {}),
+                    'headers': dict(self.request.headers)},
+                'logger': 'sprockets.mixins.sentry',
+                'modules': self._get_module_data()}
+            kwargs['extra']['http_host'] = self.request.host
+            kwargs['extra']['remote_ip'] = self.request.remote_ip
+
+        if self.sentry_tags:
+            kwargs.update({'tags': self.sentry_tags})
+        self.sentry_client.captureException(True, **kwargs)
+
         super(SentryMixin, self)._handle_request_exception(e)
 
     def _get_module_data(self):
@@ -97,3 +97,7 @@ class SentryMixin(object):
             return pkg_resources.get_distribution(module_name).version
         except pkg_resources.DistributionNotFound:
             return None
+
+    @property
+    def sentry_client(self):
+        return getattr(self.application, SENTRY_CLIENT, None)
