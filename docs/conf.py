cd67f00f0c114ca3012dcc4798dc8a084d28ae8f
Andrew Rabert <andrewr@aweber.com>
2018-12-05 10:18:07 -0500

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ extensions = [
 ]
 master_doc = 'index'
 project = 'sprockets.mixins.sentry'
-copyright = '2016, AWeber Communications'
+copyright = '2016-2018, AWeber Communications'
 release = pkg_resources.get_distribution('sprockets.mixins.sentry').version
 version = '.'.join(release.split('.')[0:1])
 pygments_style = 'sphinx'
